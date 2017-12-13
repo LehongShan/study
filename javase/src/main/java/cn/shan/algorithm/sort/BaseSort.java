45452b156a8cc68aa21090cbd53ba05a861fb6a6
LehongShan <419732717@qq.com>
2017-12-13 11:58:13 +0800

    排序

--- javase/src/main/java/cn/shan/algorithm/sort/BaseSort.java
+++ javase/src/main/java/cn/shan/algorithm/sort/BaseSort.java
@@ -1,7 +1,5 @@
 package cn.shan.algorithm.sort;
 
-import org.junit.Test;
-
 /**
  * Created by shanlehong on 2017/12/12.
  */
--- javase/src/main/java/cn/shan/algorithm/sort/SortClient.java
+++ javase/src/main/java/cn/shan/algorithm/sort/SortClient.java
@@ -3,14 +3,17 @@ package cn.shan.algorithm.sort;
 import org.junit.Test;
 
 /**
+ * 排序触发客户端
  * Created by shanlehong on 2017/12/12.
  */
-public class TestSort {
+public class SortClient {
 
     @Test
-    public void testBubbleSort() {
+    public void testSort() {
         BaseSort.print();
         BubbleSort.sort();
         QuickSort.sort();
+        SearchSort.sort();
+        SearchSort.sort2();
     }
 }
