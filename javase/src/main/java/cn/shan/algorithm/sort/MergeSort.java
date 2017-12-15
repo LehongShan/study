cc3913bca39794aa141fa1b149024b136289430b
LehongShan <419732717@qq.com>
2017-12-15 10:00:36 +0800

    归并排序

--- javase/src/main/java/cn/shan/algorithm/sort/MergeSort.java
+++ javase/src/main/java/cn/shan/algorithm/sort/MergeSort.java
@@ -29,24 +29,36 @@ public class MergeSort extends BaseSort {
 
     }
 
+    /**
+     * 将两个数组进行归并，归并前两个数组已经有序，归并后依然有序
+     * @param a 数组对象
+     * @param left 左数组的第一个元素的索引
+     * @param center 左数组的最后一个元素的索引，center+1 为右数组的第一个元素的索引
+     * @param right 右数组的最后一个元素的索引
+     */
     private static void merge(int [] a,int left,int center,int right){
         int [] temp = new int[a.length];
         int middle = center +1;
+        //记录中间数组的索引
         int thirdIndex = left;
         int tempIndex = left;
         while (left<=center && middle<=right){
+            //从两个数组中取出小的放入中间数组
             if (a[left]<= a[middle]){
                 temp[thirdIndex++] = a[left++];
             }else {
                 temp[thirdIndex++] = a[middle++];
             }
         }
+        //剩余部分依次放入中间数组
         while(middle<=right){
             temp[thirdIndex++] = a[middle++];
         }
         while (left<=center){
             temp[thirdIndex++] = a[left++];
         }
+        //将中间数组中的内容复制拷回原数组
+        //(原left-right范围的内容被复制回原数组)
         while(tempIndex<=right){
             a[tempIndex] = temp[tempIndex++];
         }
