4c7c3e1a837361bdc66755910a70f106c0c49c1f
LehongShan <419732717@qq.com>
2017-12-18 11:37:06 +0800

    链栈实现

--- javase/src/main/java/cn/shan/satck/StackClient.java
+++ javase/src/main/java/cn/shan/satck/StackClient.java
@@ -22,4 +22,19 @@ public class StackClient {
         sequenceStack.printStack();
     }
 
+    @Test
+    public void testLinkStack(){
+        LinkStack<String> linkStack = new LinkStack<>();
+        linkStack.push("aaaaa");
+        linkStack.push("bbbbb");
+        linkStack.push("ccccc");
+        linkStack.push("ddddd");
+        linkStack.printStack();
+        System.out.println("栈顶元素："+linkStack.peek());
+        System.out.println("出栈元素："+linkStack.pop());
+        System.out.println("入栈元素：eeeee");
+        linkStack.push("eeeee");
+        linkStack.printStack();
+    }
+
 }
